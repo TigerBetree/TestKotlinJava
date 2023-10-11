61df27bff21632a856a36e444493b7097bac0931
Tiger <tiger.betree@gmail.com>
2023-10-11 21:58:59 +0800

    Default Changelist

---
+++ src/com/test/java/thread/TestMeituan.java
@@ -0,0 +1,81 @@
+package com.test.java.thread;
+
+import java.util.ArrayList;
+
+public class TestMeituan {
+
+    public static void main(String[] args) {
+        CP cp1 = new CP("test1", 10, 101);
+        CP cp2 = new CP("test2", 20, 101);
+        CP cp3 = new CP("test3", 30, 102);
+        ArrayList cpList = new ArrayList<CP>();
+        cpList.add(cp1);
+        cpList.add(cp2);
+        cpList.add(cp3);
+
+        Youhui youhui1 = new Youhui(101, 2);
+        Youhui youhui2 = new Youhui(102, 3);
+        ArrayList youhuiList = new ArrayList<Youhui>();
+        youhuiList.add(youhui1);
+        youhuiList.add(youhui2);
+
+        float discount = 0.5f;
+
+        System.out.println(count(cpList, youhuiList, discount));
+    }
+
+    static class CP {
+        String name;
+        float price;
+        int youhuiId;// 101, 102
+
+        public CP(String name, float price, int youhuiId){
+            this.name = name;
+            this.price = price;
+            this.youhuiId = youhuiId;
+        }
+    }
+
+    static class Youhui {
+        int id;
+        int discount;
+
+        public Youhui(int id, int discount){
+            this.id = id;
+            this.discount = discount;
+        }
+    }
+
+    public static float count(ArrayList<CP> cpList, ArrayList<Youhui> youhuiList, float discount){
+        if(cpList.isEmpty()){
+            return 0;
+        }
+
+        float totleMoney = 0l;
+
+        for(int i = 0; i < cpList.size(); i++){
+            CP cp = cpList.get(i);
+            totleMoney += getPriceAfterYouhui(cp, youhuiList);
+        }
+
+        return totleMoney * discount;
+    }
+
+    public static float getPriceAfterYouhui(CP cp, ArrayList<Youhui> youhuiList) {
+        if(youhuiList.isEmpty()){
+            return cp.price;
+        }
+
+        for(int i = 0; i < youhuiList.size(); i++){
+            Youhui youhui = youhuiList.get(i);
+            if(cp.youhuiId == youhui.id){
+                return cp.price - youhui.discount;
+            }
+        }
+
+        return cp.price;
+    }
+
+}
+
+
